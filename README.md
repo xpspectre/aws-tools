1295a54a21344a75cf62f8b211e06f53775d7a63
Kevin Shi <xpspectre@gmail.com>
2016-12-07 22:46:38 -0500

    Add readme and license. Switch instances list of lists to dicts,

---
+++ README.md
@@ -0,0 +1,3 @@
+# aws-tools
+
+Misc tools for AWS, including spot market cluster deployment scripts
--- instance_calc.py
+++ instance_calc.py
@@ -21,23 +21,24 @@ with open(INSTANCE_TABLE, 'r') as csvfile:
             ecu = float('nan')
         mem = float(row[3])
         disk = row[4]  # arbitrary string for EBS, multiple SSDs, etc
-        instances.append([name, cpu, ecu, mem, disk])
+        instances.append({'name': name, 'cpu': cpu, 'ecu': ecu, 'mem': mem, 'disk': disk})
 
 print(instances)
 
 # Request resources
 #   Get list of instances and the number of units each instance type can give you
 #   Doesn't explicitly pay attention to inefficient packing - this will get sorted out depending on price/unit
-resource_req = {'cpu': 1, 'mem': 2.0}  # constitutes a single "unit" of request
+resource_req = {'cpu': 8, 'mem': 16.0}  # constitutes a single "unit" of request
 
 instance_units = []
 for type in instances:
-    ncpu = math.floor(type[1]/resource_req['cpu'])  # how many units can pack into CPUs
-    nmem = math.floor(type[3]/resource_req['mem'])  # how many units can pack into memory
+    ncpu = math.floor(type['cpu']/resource_req['cpu'])  # how many units can pack into CPUs
+    nmem = math.floor(type['mem']/resource_req['mem'])  # how many units can pack into memory
     n = min(ncpu, nmem)
     if n >= 1:
-        name = type[0]
-        instance_units.append([name, n])
+        name = type['name']
+        instance_units.append({'name': name, 'nunits': n})
 
-print(instance_units)
-
+# print(instance_units)
+for type in instance_units:
+    print(type)
